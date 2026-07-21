0f06938f17f06b0b98db3a4860d6854186a86e85
coderaiser <coderaiser@users.noreply.github.com>
2026-07-21 12:31:22 +0000

    chore: redrun: actions: lint ☘️

--- bin/putnik.js
+++ bin/putnik.js
@@ -1,10 +1,11 @@
-import {loadSqlPlugin} from '../lib/putnik.js';
-import putnik from '../lib/putnik.js';
+import putnik, {loadSqlPlugin} from '../lib/putnik.js';
 
 const pluginConstToLet = loadSqlPlugin(new URL('../lib/plugins/const-to-let.sql', import.meta.url).pathname);
 
 const [code, places] = await putnik('src/index.js', 'const a = 1;', {
-    plugins: [['const-to-let', pluginConstToLet]],
+    plugins: [
+        ['const-to-let', pluginConstToLet],
+    ],
 });
 
 console.log(code);
