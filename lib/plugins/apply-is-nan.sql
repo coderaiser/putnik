cff6451aada4e571a74d6eb380cfd91fc413ed9d
coderaiser <coderaiser@cloudcmd.io>
2026-08-17 19:29:15 +0000

    feature: sql-plugin: simplify

--- lib/plugins/apply-is-nan.sql
+++ lib/plugins/apply-is-nan.sql
@@ -1,40 +1,40 @@
 -- @select
 SELECT
-    id,
-    start_line,
-    start_col,
-    parent_id,
-    parent_type,
-    parent_field
-FROM BinaryExpression
-WHERE operator = '==='
-AND file = :file;
+    bin.id,
+    bin.start_line,
+    bin.start_col,
+    bin.parent_id,
+    bin.parent_type,
+    bin.parent_field,
+    arg_id.name AS arg_name
+FROM BinaryExpression bin
+JOIN Identifier nan_id
+    ON nan_id.parent_id = bin.id
+    AND nan_id.name = 'NaN'
+JOIN Identifier arg_id
+    ON arg_id.parent_id = bin.id
+    AND arg_id.id != nan_id.id
+WHERE bin.operator = '===';
 
 -- @report
 SELECT 'Prefer Number.isNaN over === NaN' AS message;
 
 -- @fix
 DELETE FROM Identifier WHERE parent_id = :id AND name = 'NaN';
-
 DELETE FROM BinaryExpression WHERE id = :id;
-
 INSERT INTO CallExpression (file, parent_id, parent_type, parent_field)
 VALUES (:file, :parent_id, :parent_type, :parent_field)
 RETURNING id AS call_id;
-
-INSERT INTO MemberExpression (file, parent_id, parent_type, parent_field)
-VALUES (:file, :call_id, 'CallExpression', 'callee')
-RETURNING id AS member_id;
-
-INSERT INTO Identifier (file, name, parent_id, parent_type, parent_field)
-VALUES (:file, 'Number', :member_id, 'MemberExpression', 'object');
-
-INSERT INTO Identifier (file, name, parent_id, parent_type, parent_field)
-VALUES (:file, 'isNaN', :member_id, 'MemberExpression', 'property');
-
 UPDATE Identifier
 SET parent_id = :call_id,
     parent_type = 'CallExpression',
-    parent_field = 'arguments.0'
+    parent_field = 'arguments'
 WHERE parent_id = :id
-AND name != 'NaN';
+AND name = :arg_name;
+INSERT INTO MemberExpression (file, parent_id, parent_type, parent_field)
+VALUES (:file, :call_id, 'CallExpression', 'callee')
+RETURNING id AS member_id;
+INSERT INTO Identifier (file, name, parent_id, parent_type, parent_field)
+VALUES (:file, 'Number', :member_id, 'MemberExpression', 'object');
+INSERT INTO Identifier (file, name, parent_id, parent_type, parent_field)
+VALUES (:file, 'isNaN', :member_id, 'MemberExpression', 'property');
