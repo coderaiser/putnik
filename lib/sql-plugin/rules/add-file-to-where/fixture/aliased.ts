f6ed4e41baeb8a8d89cbebc47eec225c29f8a9ec
coderaiser <coderaiser@cloudcmd.io>
2026-08-19 22:58:56 +0000

    feature: putnik: as

---
+++ lib/sql-plugin/rules/add-file-to-where/fixture/aliased.ts
@@ -0,0 +1,1 @@
+section('@select', select(bin.id, from(BinaryExpression as 'bin', join(Identifier as 'nan_id', on(nan_id.parent_id === bin.id))), where(bin.operator === '===')));
---
+++ lib/sql-plugin/rules/add-location-to-select/fixture/with-fix.ts
@@ -0,0 +1,3 @@
+__putout_processor_sql([
+    section('@select', select(bin.id, bin.parent_id, bin.parent_type, bin.parent_field, arg_id.name as 'arg_name', bin.start_line, bin.start_col, from(BinaryExpression as 'bin', join(Identifier as 'nan_id', on(nan_id.parent_id === bin.id && nan_id.name === 'NaN')), join(Identifier as 'arg_id', on(arg_id.parent_id === bin.id && arg_id.id !== nan_id.id))), where(bin.operator === '==='))),
+]);
