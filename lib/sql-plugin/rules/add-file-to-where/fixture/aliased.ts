ba7abb9ec186d0a232eca60e346e2dab36478336
coderiaser <coderaiser@cloudcmd.io>
2026-08-20 15:35:39 +0300

    test: coverage

--- lib/sql-plugin/rules/add-file-to-where/fixture/aliased.ts
+++ lib/sql-plugin/rules/add-file-to-where/fixture/aliased.ts
@@ -1,6 +1,8 @@
 section('@select', select(
     bin.id,
-    from(BinaryExpression as bin),
-    join(Identifier as nan_id),
-    on(nan_id.parent_id === bin.id),
-), where(bin.operator === '==='));
+    from(
+        BinaryExpression as bin,
+        join(Identifier as nan_id), on(nan_id.parent_id === bin.id),
+        where(bin.operator === '===')
+    )
+));
