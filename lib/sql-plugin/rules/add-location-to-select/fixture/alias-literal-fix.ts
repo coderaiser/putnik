d9bcd2da50aeac659396b326893f96e0cacead5b
coderaiser <coderaiser@cloudcmd.io>
2026-08-26 17:09:20 +0000

    feature: comments

---
+++ lib/sql-plugin/rules/add-location-to-select/fixture/alias-literal-fix.ts
@@ -0,0 +1,1 @@
+section('@select', select(id, users.start_line, users.start_col, from(x as 'users')));
---
+++ lib/sql-plugin/rules/add-location-to-select/fixture/alias-reference-fix.ts
@@ -0,0 +1,1 @@
+section('@select', select(id, Users.start_line, Users.start_col, from(x as Users)));
